f2b66b64935f2020a81721ca1b1a47dfe413e731
tanin69 <tanin69@gmail.com>
2024-05-14 08:05:41 +0200

    fix : voronoi diagram was filled

--- AgriFieldGenerator/processing/data_processing_base_class.py
+++ AgriFieldGenerator/processing/data_processing_base_class.py
@@ -14,13 +14,7 @@ class DataProcessingBaseClass:
 
     def transform(self, input_data):
         raise NotImplementedError("Subclasses should implement this!")
- 
-    # todo : modifier la méthode display pour qu'elle puisse afficher 
-    # les données de la méthode transform de la classe voronoi_filler
-    # plutôt que de tester le type de données pour afficher les points
-    # ou les polygones, il faudrait charger les données de polygon.pkl,
-    # points.pkl et voronoi_fill.pkl et les afficher
-    
+   
     def display(self, data):
         # Load data from pickle files
         with open(self.save_data_path + 'polygon.pkl', 'rb') as f:
@@ -29,62 +23,42 @@ class DataProcessingBaseClass:
             points = pickle.load(f)
         with open(self.save_data_path + 'voronoi_fill.pkl', 'rb') as f:
             voronoi_fill = pickle.load(f)
+        
+        # Close all existing figures
+        plt.close('all')
 
+        # Create a new figure and axes
+        fig, ax = plt.subplots()
+        
+        # Set the limits of the axes to the SVG dimensions
+        ax.set_xlim(0, self.svg_width)
+        ax.set_ylim(0, self.svg_height)
+        
         # Display polygon
         if isinstance(polygon, Polygon):
             x, y = polygon.exterior.xy
-            plt.fill(x, y, alpha=0.5, fc='r', ec='none')
+            ax.fill(x, y, alpha=0.5, fc='r', ec='none')
         elif isinstance(polygon, MultiPolygon):
             for poly in polygon.geoms:
                 x, y = poly.exterior.xy
-                plt.fill(x, y, alpha=0.5, fc='r', ec='none')
-
+                ax.plot(x, y, color='r')
+        
         # Display points
         for point in points:
-            plt.plot(*point, 'ko')
-
+            ax.plot(*point, 'ko')
+        
         # Display Voronoi fill
         for polygon in voronoi_fill:
             if isinstance(polygon, Polygon):
                 x, y = polygon.exterior.xy
-                plt.fill(x, y, alpha=0.4, fc='b', ec='none')
+                ax.plot(x, y, color='b')
             elif isinstance(polygon, MultiPolygon):
                 for poly in polygon.geoms:
                     x, y = poly.exterior.xy
-                    plt.fill(x, y, alpha=0.4, fc='b', ec='none')
-
+                    ax.plot(x, y, color='b')
+        
         plt.show()
-
-    """
-    def display(self, data):
-        fig, ax = plt.subplots()
-        ax.set_ylim(0, self.svg_height)  # Set the y-axis limits to match the SVG height
-        ax.set_xlim(0, self.svg_width)  # Set the x-axis limits to match the SVG width
-    
-        if isinstance(data, tuple):  # If data is a tuple of (polygon, points)
-            polygon, points = data
-            # Display the points
-            if points:
-                ax.scatter(*zip(*points), color='b', s=5)
-            # Display the polygon
-            if isinstance(polygon, Polygon):
-                x, y = polygon.exterior.xy
-                ax.plot(x, y, color='red')
-            elif isinstance(polygon, MultiPolygon):
-                for poly in polygon.geoms:
-                    x, y = poly.exterior.xy
-                    ax.plot(x, y, color='red')
-        elif isinstance(data, MultiPolygon):
-            for polygon in data.geoms:
-                x, y = polygon.exterior.xy
-                ax.plot(x, y, color='red')
-        elif isinstance(data, Polygon):
-            x, y = data.exterior.xy
-            ax.plot(x, y, color='red')
-    
-        plt.show()
-    """
-    
+  
     def save(self, result, filename, data_file=False):
         if data_file:
             if not os.path.exists(self.save_data_directory):
