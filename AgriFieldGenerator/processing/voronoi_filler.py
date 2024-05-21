956ef0022516093caaaa6258eec8f018ad31fb55
Tanin69 <tanin69@gmail.com>
2024-05-21 17:47:00 +0200

    typos in header comments

--- AgriFieldGenerator/processing/voronoi_filler.py
+++ AgriFieldGenerator/processing/voronoi_filler.py
@@ -4,12 +4,12 @@
 # The project makes it possible to generate patterns of cultivated fields 
 # reproducing as faithfully as possible the diversity of agricultural 
 # landscapes. It allows you to generate texture masks that can be used in the
-# world editor of the Enfusion workshop.
+# world editor of the Enfusion Workbench.
 #
 # It is released under the MIT License. Please see the LICENSE file for details.
 #
 # Enfusion is a game engine developed by Bohemia Interactive.
-# The Enfusion Workshop is a creation workshop dedicated to the Enfusion engine.
+# The Enfusion Workbench is a creation workbench dedicated to the Enfusion engine.
 # 
 
 import os
